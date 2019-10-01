edc20799ec3d7939fb64026e243a83834cdba11f
Elliot Zoerner <eliot.zoerner@gmail.com>
2019-10-01 18:26:29 +0930

    updated readme

---
+++ readme.md
@@ -0,0 +1,28 @@
+# Online 2 Player Backgammon 
+
+### Running the app
+
+1. Clone this repo (or download and unzip)
+2. cd into the root directory
+3. Run `npm install`
+4. Run `npm build`. This will install the dependencies for the client, and build the client and server bundles.
+5. Run `npm start`
+6. Navigate to `localhost:3000` in your browser
+
+### How to use the game
+
+1. Go to the root endpoint (`localhost:3000` if you're running locally)
+2. Start a game
+3. Send the URL given on the page to a friend (or open it in another tab if you're testing the app yourself)
+4. Follow the on screen instructions to set a name for your player
+5. Follow the instructions to play the game
+
+The `/analytics` endpoint displays a chart that represents the progress through the active games on the server.
+
+### Example Deployment
+
+An example deployment of the app can be found at https://ez-backgammon.herokuapp.com/
+
+--- 
+
+Created by Elliot Zoerner as part of UNE's COSC360 subject.
--- server/index.ts
+++ server/index.ts
@@ -42,19 +42,6 @@ mongoose.connect(`mongodb://${user}:${pw}@ds113626.mlab.com:13626/backgammon`, (
   console.log('Connected to database');
 });
 
-// Fake data for analytics
-/* const fakeData = [
-  {"player0Id":"w74cQ-oLGsZ-n9vKAAAC","player1Id":"","name0":"","name1":"","uniqueCode0":"ngry8ffinr","uniqueCode1":"16zarp16fc",
-    "gameState":{"gamePhase":0,"player0Turn":false,"needsToRoll":false,"initialDice0":4,"initialDice1":5,"dice":[4,5],"movesLeft":[4,5],
-      "pieces":[[0,0,11,11,11,11,11,16,16,16,18,18,18,18,18],[5,5,5,5,5,7,7,7,12,12,12,12,12,23,23]]}},
-  {"player0Id":"FPkAI2NOA2P2vG5mAAAD","player1Id":"KJG8Q1iDyebOKTwGAAAE","name0":"Elliot","name1":"Sam","uniqueCode0":"s0tj4l030i","uniqueCode1":"is8aq5tdif",
-    "gameState":{"gamePhase":2,"player0Turn":true,"needsToRoll":true,"initialDice0":-1,"initialDice1":-1,"dice":[-1,-1],"movesLeft":[-1,-1],"pieces":
-      [[0,11,11,11,15,15,14,16,16,20,22,22,19,19,20],[1,4,4,5,5,1,7,7,24,12,12,12,12,12,17]]}},
-  {"player0Id":"GhI3qAIeW7rjIx3SAAAF","player1Id":"7S4qpu6ofz9N1gwdAAAG","name0":"John","name1":"setname","uniqueCode0":"jxzg8mtuea","uniqueCode1":"nzkj3mh1pc",
-    "gameState":{"gamePhase":2,"player0Turn":false,"needsToRoll":true,"initialDice0":-1,"initialDice1":-1,"dice":[-1,-1],"movesLeft":[-1,-1],
-      "pieces":[[0,0,11,11,11,11,17,16,16,17,18,18,18,18,18],[5,5,5,5,5,7,7,7,12,12,12,12,12,23,23]]}}
-]; */
-
 // Volatile storage of games
 export const gamesBeingPlayed:GameI[] = [];
 
